d55b76b859964333a2f1c2a4b3fc959d2430e70e
unknown <difalcoalex@gmail.com>
2024-01-22 19:12:15 +0100

    Primo inserimento in Git

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    i18n: {
+        // i locali supportati dall'applicazione
+        locales: ['en', 'it', 'es'],
+        // il locale predefinito da usare quando si visita
+        // un percorso non localizzato (per esempio `/about`)
+        defaultLocale: 'en'
+    },
+}
